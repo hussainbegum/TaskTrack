b1201f1796a63a4f0c8361c695af24b5910dc948
Chganti <chaganti0917@gmail.com>
2026-03-26 09:46:05 +0530

    Remove Task_Frontend from repository

--- Task_Frontend/src/app/admin/users-list/users-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-users-list',
-  imports: [],
-  templateUrl: './users-list.html',
-  styleUrl: './users-list.css',
-})
-export class UsersList {}
